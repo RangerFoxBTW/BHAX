1ebc039c0d5de9b5546c36741a1fbf212e3e2d7a
Dani Szabó <daniszabobtw@gmail.com>
2020-04-04 14:24:17 +0200

    progress demands sacrifices

---
+++ thematic_tutorials/bhax_textbook_IgyNeveldaProgramozod/src/Welch/polargen.h
@@ -0,0 +1,24 @@
+// POLARGEN.H
+#ifndef POLARGEN__H
+#define POLARGEN__H
+#include <cstdlib>
+#include <cmath>
+#include <ctime>
+
+class PolarGen
+{
+    public: 
+        PolarGen()
+        {
+            nincsTarolt = true;
+            std::srand (std::time (NULL));
+        }
+        ~PolarGen(){}
+        double kovetkezo();
+
+    private:
+        bool nincsTarolt;
+        double tarolt;
+};
+
+#endif
