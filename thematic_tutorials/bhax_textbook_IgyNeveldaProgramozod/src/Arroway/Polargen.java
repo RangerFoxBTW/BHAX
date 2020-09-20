da6c379c0790b2a0b3f63cb45537c52ffdf672cb
Dani Szabó <daniszabobtw@gmail.com>
2020-09-20 20:22:49 +0200

    No f***ing way! Arroway is done!

---
+++ thematic_tutorials/bhax_textbook_IgyNeveldaProgramozod/src/Arroway/Polargen.java
@@ -0,0 +1,50 @@
+public class Polargen {
+    
+    boolean notStored = true;
+    double stored;
+    
+    public Polargen() {
+        
+        notStored = true;
+        
+    }
+    
+    public double következő() {
+        
+        if(notStored) {
+            
+            double u1, u2, v1, v2, w;
+            do {
+                u1 = Math.random();
+                u2 = Math.random();
+                
+                v1 = 2*u1 - 1;
+                v2 = 2*u2 - 1;
+                
+                w = v1*v1 + v2*v2;
+                
+            } while(w > 1);
+            
+            double r = Math.sqrt((-2*Math.log(w))/w);
+            
+            stored = r*v2;
+            notStored = !notStored;
+            
+            return r*v1;
+            
+        } else {
+            notStored = !notStored;
+            return stored;
+        }
+    }
+    
+    public static void main(String[] args) {
+        
+        Polargen g = new Polargen();
+        
+        for(int i=0; i<10; ++i)
+            System.out.println(g.következő());
+        
+    }
+    
+}
